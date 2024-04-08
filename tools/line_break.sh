8993914c8586fb77c3154830d8fec671a88acf28
tcheukueppo <tcheukueppo@yandex.com>
2024-04-08 01:55:56 +0000

    tools: line_break.sh to properly breaks lines by setting a max line length.

---
+++ tools/line_break.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+perl -E 'local $/; $t = <> =~ s/\n+/ /gr; $t =~ s/\s+$//; say ((length((split " ", substr($t, pos($t) - 1, 2))[0]) == 2 ? scalar(pos($t) = pos($t) - (length($1) - length($l = $1 =~ s/\S+$//r)), $l) : $1) =~ s/^\s+//r =~ s/\s+$//r) while $t =~ /\G(.{1,'${1:-74}'})/g'
